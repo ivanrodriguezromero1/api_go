47b0e3ac92cc53d96683722a83ca39f0c4c337dc
ivan <ivanrr1991@gmail.com>
2023-03-06 11:23:13 -0500

    ini3.9

--- servicio/main.go
+++ servicio/main.go
@@ -1,45 +1,28 @@
 package main
 
 import (
+	"api_raz_mat/internal/handlers"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
 
-	// r := mux.NewRouter()
-	// r.HandleFunc("/books", handlers.GetBooks).Methods("GET")
-	// r.HandleFunc("/books", handlers.AddBook).Methods("POST")
-	// c := cors.New(cors.Options{
-	// 	AllowedOrigins: []string{"*"},
-	// 	AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	// 	AllowedHeaders: []string{"Accept", "Content-Length", "Accept-Encoding"},
-	// })
-	// handler := c.Handler(r)
-	// fmt.Println("Servidor iniciado")
-	// if err := http.ListenAndServe(":8000", handler); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Permitimos todas las solicitudes de cualquier origen
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-
-		if r.Method == "OPTIONS" {
-			// Agregamos los headers CORS necesarios para las solicitudes OPTIONS
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		// Enviamos la respuesta normal
-		fmt.Fprint(w, "Hola, mundo!")
+	r := mux.NewRouter()
+	r.HandleFunc("/books", handlers.GetBooks).Methods("GET")
+	r.HandleFunc("/books", handlers.AddBook).Methods("POST")
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Content-Length", "Accept-Encoding"},
 	})
-
+	handler := c.Handler(r)
 	fmt.Println("Servidor iniciado")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+	if err := http.ListenAndServe(":8000", handler); err != nil {
+		log.Fatal(err)
 	}
-
 }
